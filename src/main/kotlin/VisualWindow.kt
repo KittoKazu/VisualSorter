4246333ca4405ca700fefeac5fc58eb2bc9aec61
KittoKazu <ajmstaring@gmail.com>
2025-07-01 18:43:25 +0200

    Added input parsing functions

--- src/main/kotlin/VisualWindow.kt
+++ src/main/kotlin/VisualWindow.kt
@@ -61,3 +61,20 @@ class MainApp : Application() {
         stage.show()
     }
 }
+/*
+Checks whether the string only contains usable characters. Returns True or False
+ */
+fun CheckIfUsable(input: String): Boolean {
+    val filter = Regex("^[\\d,\\s]*$") //regex expression filters out d(digits), commas and s(whitespaces)
+    return input.matches(filter)
+}
+
+/*
+Converts a string into a list, ignoring spaces.
+ */
+fun ConvertToList(input:String): List<Int> {
+    return input
+        .split(",") //Filters values seperated by comma
+        .map {it.trim()}.map{it.isNotEmpty()} //Filters out spaces before and after values
+        .map {it.toInt()} //Turns the values to Int
+}
